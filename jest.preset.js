854934a24735d35c65be86ec4609cfdfa68020de
Ricardo Javier Barrios Díaz <62288249+RicardoJBarriosEviden@users.noreply.github.com>
2023-08-27 21:02:56 +0100

    chore: add git lint config

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,7 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset, setupFilesAfterEnv: [...(nxPreset.setupFilesAfterEnv ?? []), 'jest-extended/all'] };
+module.exports = {
+  ...nxPreset,
+  setupFilesAfterEnv: [...(nxPreset.setupFilesAfterEnv ?? []), 'jest-extended/all'],
+  coverageReporters: [...(nxPreset.coverageReporters ?? []), 'lcov']
+};
