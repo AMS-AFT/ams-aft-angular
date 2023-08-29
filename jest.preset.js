fe7430a70b038c0336f4a631f1c17de295b65498
Ricardo Javier Barrios Díaz <62288249+RicardoJBarriosEviden@users.noreply.github.com>
2023-08-29 08:56:08 +0100

    chore: update config

--- jest.preset.js
+++ jest.preset.js
@@ -1,7 +1,5 @@
 const nxPreset = require('@nx/jest/preset').default;
 
 module.exports = {
-  ...nxPreset,
-  setupFilesAfterEnv: [...(nxPreset.setupFilesAfterEnv ?? []), 'jest-extended/all'],
-  coverageReporters: [...(nxPreset.coverageReporters ?? []), 'lcov']
+  ...nxPreset
 };
