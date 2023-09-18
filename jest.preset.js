60b191c979e166b764672ecf5a4b5193300628a4
Ricardo Javier Barrios Díaz <62288249+RicardoJBarriosEviden@users.noreply.github.com>
2023-09-18 11:31:21 +0100

    chore: setup jest and eslint

--- jest.preset.js
+++ jest.preset.js
@@ -2,5 +2,7 @@ const nxPreset = require('@nx/jest/preset').default;
 
 module.exports = {
   ...nxPreset,
-  setupFilesAfterEnv: [...(nxPreset.setupFilesAfterEnv ?? []), 'jest-extended/all']
+  setupFilesAfterEnv: [...(nxPreset.setupFilesAfterEnv ?? []), 'jest-extended/all'],
+  resetMocks: true,
+  restoreMocks: true
 };
